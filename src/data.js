4d364da535f3bb5d64d31326628fae5eddaa78fc
Ardhan Dimas Nuryadin <ardhandimas9@gmail.com>
2023-09-23 19:27:47 +0700

    commit pertama

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const projects = [
+    {
+      title: "Laravel E Commerce",
+      subtitle: "Laravel",
+      description:
+        "Tampilan sederhana dari beranda Web menggunakan web (Gambar dan projek hanya sample)",
+      image: "../assets/images/project1.png",
+      link: "https://ecommerceiq.asia/?gclid=CjwKCAjwmbqoBhAgEiwACIjzEOZvvfPxEIbQJKkS4Lqa-rKe9s94tmT6TnXOqKHXfRSdNmNu7Znn2RoCbAoQAvD_BwE",
+    },
+    {
+      title: "React Website",
+      subtitle: "React JS",
+      description:
+        "Tampilan Webisite menggunakan react js (Gambar dan projek hanya sample)",
+      image: "../assets/images/project2.png",
+      link: "https://www.cavai.com/",
+    },
+  ];
