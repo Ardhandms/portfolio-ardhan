2fb18172cb8ffbee843da9e020d7bbb927768536
Ardhan Dimas Nuryadin <ardhandimas9@gmail.com>
2024-05-19 22:28:14 +0700

    revisi 1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,7 +49,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">082320997062</p>
             </div>
           </div>
         </div>
@@ -58,50 +58,23 @@ export default function Contact() {
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Hubungi aku
           </h2>
           <p className="leading-relaxed mb-5">
-            Hubungi saya untuk keterengan lebih lanjut sebagai Front End Web Developer
+            Temukan aku di sosial mediaku.
           </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
+          <div className="relative mb-4 flex space-x-8">
+              <a href="https://www.instagram.com/ardhandms_?igsh=a3ZscDVseWMwYXQx">
+                  <img 
+                    src="../assets/images/instagram.png"
+                  />
+              </a>
+              <a href="https://wa.me/082320997062">
+                  <img 
+                    src="../assets/images/whatsapp.png"
+                  />
+              </a>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
-          </button>
         </form>
       </div>
     </section>
